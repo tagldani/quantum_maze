51f9fba3bd9aeadbb84175375b095e53ea4b608b
Daniele Tagliabue <d.tagliabue@workhousesrl.com>
2026-06-04 20:47:35 +0200

    First Fragment

--- src/main.js
+++ src/main.js
@@ -9,11 +9,26 @@ function resize() {
 resize();
 
 window.addEventListener("resize", resize);
-
+const fragment = {
+    x: window.innerWidth / 2 + 120,
+    y: window.innerHeight / 2
+};
 function draw() {
 
     ctx.clearRect(0, 0, canvas.width, canvas.height);
+ctx.fillStyle = "#00d4ff";
 
+ctx.beginPath();
+
+ctx.arc(
+    fragment.x,
+    fragment.y,
+    8,
+    0,
+    Math.PI * 2
+);
+
+ctx.fill();
     ctx.fillStyle = "white";
 
     const time = Date.now() * 0.003;
